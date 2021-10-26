a466557c074bf44875ad36d88c948b2bed8577ec
Md Joynal Abdin Robel <robel.joynal@gmail.com>
2021-10-26 23:45:52 +0600

    done

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>Not Found</h1>
+        <div className="not-found">
+            <h1>404</h1>
+            <h2>Opps!!! Page Not Found</h2>
         </div>
     );
 };
